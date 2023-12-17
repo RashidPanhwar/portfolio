d39df20fed7c601cff40d25c6e6deb92a61fc0f2
RashidPanhwar <rashidpanhwer8@gmail.com>
2023-12-17 23:56:40 +0500

    Add accounts links to social icons and add image in title

--- src/portfolio/SocialLinks.jsx
+++ src/portfolio/SocialLinks.jsx
@@ -1,60 +1,17 @@
 import React from 'react'
-import {FaFacebook, FaLinkedin} from 'react-icons/fa'
+import {FaFacebook, FaGithub, FaLinkedin} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi';
 import {BsFillPersonLinesFill} from 'react-icons/bs';
 
 const SocialLinks = () => {
     
     const myIcons = [
-        {name: "Facebook", icon: <FaFacebook size={30} />, href: '#facebook', style: 'rounded-tr-md'},
-        {name: "LinkedIn", icon: <FaLinkedin size={30} />, href: '#facebook'},
+        {name: "LinkedIn", icon: <FaLinkedin size={30} />, href: 'https://www.linkedin.com/in/rashid-ali-88a4ba122/', style: 'rounded-tr-md'},
+        {name: "Github", icon: <FaGithub size={30} />, href: 'https://github.com/RashidPanhwar'},
         {name: "Mail", icon: <HiOutlineMail size={30} />, href: 'mailto:rashidpanhwer8@gmail.com'},
         {name: "Resume", icon: <BsFillPersonLinesFill size={30} />, href: '/mernStack.pdf', style: 'rounded-br-md', download: true},
     ]
-    // const socialIcons = [
-    //     {
-    //         id:1,
-    //         child: (
-    //             <>
-    //                 Facebook <FaFacebook  size={30}/>
-    //             </>
-    //             ),
-    //         href:"#facebook",
-    //         style: 'rounded-tr-md'
-    //     },
-    //     {
-    //         id:2,
-    //         child: (
-    //             <>
-    //                 Linkdin <FaLinkedin size={30}/>
-    //             </>
-    //         ),
-    //         href:"#facebook",
-    //     },
-    //     {
-    //         id:3,
-    //         child: (
-    //             <>
-    //                 Mail <HiOutlineMail size={30}/>
-    //             </>
-    //         ),
-    //         href:"mailto:foo@gmail.com",
-    //     },
-    //     {
-    //         id:4,
-    //         child: (
-    //             <>
-    //                 Resume <BsFillPersonLinesFill />
-    //             </>
-    //         ),
-    //         href:"/Resume.pdf",
-    //         style: 'rounded-tr-md',
-    //         download: true
 
-    //     },
-
-        
-    // ]
     return (
     <div className='hidden lg:flex flex-col fixed left-0 top-[35%]'>
         <ul >
